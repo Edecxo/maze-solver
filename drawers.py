f1d516af09567f8fd69b078a42426cb965f1e25b
Edecxo <hunterlogan.eden@gmail.com>
2025-02-17 17:50:57 -0600

    added logic to draw maze with cells inside

--- drawers.py
+++ drawers.py
@@ -33,6 +33,15 @@ class Point():
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+        
+    def __add__(self, other):
+        return Point(
+                self.x + other.x,
+                self.y + other.y
+        )
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
 
 class Line():
     def __init__(self, point_a, point_b):
@@ -89,6 +98,15 @@ class Cell():
         line = Line(from_cell_center, to_cell_center)
         line.draw(canvas, fill_color)
 
+    def update(self):
+        self._x1 = self.top_left.x
+        self._y1 = self.top_left.y
+        self._x2 = self.bottom_right.x
+        self._y2 = self.bottom_right.y
+     
+    def __repr__(self):
+        return f"Cell(({self._x1}, {self._y1}), ({self._x2}, {self._y2}))"
+
 class Maze():
     def __init__(
         self,
@@ -99,6 +117,7 @@ class Maze():
         cell_size_x,
         cell_size_y,
         win,
+        offset=Point(2, 2)
     ):
         self._x1 = x1
         self._y1 = y1
@@ -107,8 +126,51 @@ class Maze():
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
+        self.offset = offset
+        self.maze_size = None #created with _draw_cell()
         self._cells = []
         self._create_cells()
 
     def _create_cells(self):
-        pass
+        for i in range(self.num_cols):
+            self._cells.append([])
+        cell_top_left = self.offset
+        cell_bottom_right = self.offset
+        for cell in self._cells:
+            for i in range(self.num_rows):
+                cell.append(Cell(cell_top_left, cell_bottom_right))
+
+        new_y = 0
+        for row in self._cells:
+            new_x = 0
+            for cell in row:
+                cell.top_left = Point(
+                        cell._x1 + new_x,
+                        cell._y1 + new_y
+                )
+                cell.bottom_right = Point(
+                        cell.top_left.x + self.cell_size_x,
+                        cell.top_left.y + self.cell_size_x
+                )
+                new_x += self.cell_size_x
+                cell.update()
+            new_y += self.cell_size_y
+
+    def _draw_cell(self, i, j):
+        self.maze_size = Point(j, i)
+        self._animate()
+
+    def _animate(self):
+        maze = Cell(self.offset, self.maze_size + self.offset)
+        self.win.draw_cell(maze, "black")
+        self.win.redraw()
+        self.print_cells()
+
+    def print_cells(self):
+        import time
+        for row in self._cells:
+            for cell in row:
+                self.win.draw_cell(cell, "black")
+                self.win.redraw()
+                time.sleep(0.01)
+        self.win.wait_for_close()
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from maze import Maze        
+from drawers import *
 
 def main():
     win = Window(800, 600) 
+    '''
     cell1 = Cell(Point(20,20), Point(40, 40))
     cell2 = Cell(Point(50, 20), Point(70, 40))
     win.draw_cell(cell1, "black")
     win.draw_cell(cell2, "black")
     cell1.cell_move(win.canvas, cell2)
     win.wait_for_close()
+    '''
+    maze = Maze(0, 0, 10, 10, 20, 20, win)
+    maze._draw_cell(200, 200)
 
 main()
